c2f2f92aeb8426e79ffcc4bcedef13a81830e98f
trey368s <84042739+trey368s@users.noreply.github.com>
2022-09-25 16:37:56 -0400

    Update DTO

--- src/main/java/dto/bars.java
+++ src/main/java/dto/bars.java
@@ -3,11 +3,13 @@ package dto;
 import lombok.Data;
 
 public @Data
-class MarketData {
+class bars {
     private String symbol;
     private String time;
     private double open;
     private double high;
     private double low;
     private double close;
+    private int volume;
+    private String timestamp;
 }
---
+++ src/main/java/dto/trades.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.Data;
+
+public @Data
+class trades {
+    private String type;
+    private String symbol;
+    private int id;
+    private String exchange;
+    private double price;
+    private int size;
+    private String timestamp;
+    private String condition[];
+    private String tape;
+}
