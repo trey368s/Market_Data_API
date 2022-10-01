b71f378bb79b422f60e67dd9ff137fd52ece94c4
Andrew Glanz <andrewvita0@gmail.com>
2022-10-01 16:21:16 -0400

    Testing a push to Code Review branch. Removed two unused imports from MarketDataController.java to reduce load while booting program.

--- src/main/java/com.marketdata.api/MarketDataController.java
+++ src/main/java/com.marketdata.api/MarketDataController.java
@@ -1,7 +1,5 @@
 package com.marketdata.api;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
