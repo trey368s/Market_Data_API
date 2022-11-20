c6ae7b60eff85be1629499cc2c01f90a4b3697e0
trey368s <84042739+trey368s@users.noreply.github.com>
2022-11-20 11:22:55 -0500

    Add JavaDoc

--- src/main/java/com/investmenttracker/enterprise/dto/investment.java
+++ src/main/java/com/investmenttracker/enterprise/dto/investment.java
@@ -7,6 +7,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * id: Primary key holding investment id
+ * symbol: ticker symbol
+ * shares: the amount of shares purchased
+ * PriceOpened: Price that the investment was opened
+ * PriceClosed: Price that the investment was closed
+ * openTimestamp: Timestamp that the investment was opened
+ * closedTimestamp: Timestamp that the investment was closed
+ * profit: (priceClosed * shares) - (priceOpened * shares)
+ */
 @Entity
 public @Data
 class investment {
