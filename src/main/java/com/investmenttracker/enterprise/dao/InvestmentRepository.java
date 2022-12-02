acade6c9f4ae37106cf4ce6f043cc6169c837de4
Brendan Payne <payneb2@mail.uc.edu>
2022-12-02 14:55:24 -0500

    Created the InvestmentRepository file

--- src/main/java/com/investmenttracker/enterprise/dao/InvestmentRepository.java
+++ src/main/java/com/investmenttracker/enterprise/dao/InvestmentRepository.java
@@ -1,7 +1,12 @@
 package com.investmenttracker.enterprise.dao;
-/*
+
 import com.investmenttracker.enterprise.dto.Investment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface InvestmentRepository extends CrudRepository<Investment, Integer> {
-}*/
+
+    List<Investment> findByUserId(int userId);
+
+}
